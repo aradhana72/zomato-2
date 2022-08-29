685466b07d238d9cd0aca57a694596820eda0ed0
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2022-08-29 23:48:38 +0530

    Update index.js

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -1,25 +1,12 @@
-import React, {useState, useEffect} from "react";
-
-import {useSelector} from "react-redux";
-//componenets
+import React from "react";
 import DeliveryCarousal from "./DeliveryCarousal";
 
 const Delivery = () => {
-const [restaurantList, setRestaurantList] = useState([]);
-
-const reduxState = useSelector(
-  (globalStore) => globalStore.restaurant.restaurants
-);
-
-useEffect(()=>{
-  setRestaurantList(reduxState.restaurants);
-}, [reduxState.restaurants]);
-
   return (
     <>
-    <DeliveryCarousal />
+      <DeliveryCarousal />
     </>
-  );
+  )
 };
 
 export default Delivery;
