3d909958c4a81c4c509ae7b8042be9e5369eda0b
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2022-08-29 23:51:34 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
-//HOC
-import HomeLayoutHOC from "./HOC/Home.HOC";
-
-//Component
+import React from "react";
+import HomeLayoutHOC from "./HOC/Home.Hoc";
 import Temp from "./Components/temp";
-import Master from "./Components/master";
 
 function App() {
   return (
     <>
-    <HomeLayoutHOC path="/" exact component={Temp}/>
-    <HomeLayoutHOC path="/:type" exact component={Master}/>
+
+      <HomeLayoutHOC path="/" exact component={Temp} />
     </>
-  )
+  );
 }
 
 export default App;
-//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 token xyzzzzzzzzzz
